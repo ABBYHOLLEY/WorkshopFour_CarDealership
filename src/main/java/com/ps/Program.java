8987750121729ab848714868423dd2eeca95891f
Abigal Holley <abbyholley10@gmail.com>
2024-10-24 14:27:09 -0500

    added program

---
+++ src/main/java/com/ps/Program.java
@@ -0,0 +1,8 @@
+package com.ps;
+
+public class Program {
+    public static void main(String[] args) {
+        UserInterface.display();
+        // That is all I need in this file!!!!!!!
+    }
+}
---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,14 @@
+package com.ps;
+
+public class UserInterface {
+    // private static Dealership dealership;
+    public static void init(){
+        System.out.println("Init method");
+        // Loading dealership and inventory from file
+    }
+    public static void display(){
+        init();
+        System.out.println("Load menu");
+        // Load menu
+    }
+}
