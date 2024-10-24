ce780bbfeb2cef38de5f6777505c5a0b888900fc
Abigal Holley <abbyholley10@gmail.com>
2024-10-24 15:54:48 -0500

    Added inventory of vehicles and constructors, getters and setters

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -1,6 +1,24 @@
 package com.ps;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// User will be able to:
+// Find vehicles within price range
+// Find vehicle by make and model
+// Find vehicles by year range
+// Find vehicles by mileage range
+// Find vehicles by type (car, truck, suv)
+// List all vehicles
+// Add vehicle
+// Remove vehicle
+// Quit
+// Refer to leger app for help
 public class UserInterface {
+    static Scanner commandscanner = new Scanner(System.in);
+    static Scanner inputScanner = new Scanner(System.in);
+    static ArrayList<Vehicle>allVehiclesInInventory = new ArrayList<>();
+
     // private static Dealership dealership;
     public static void init(){
         System.out.println("Init method");
---
+++ src/main/java/com/ps/Vehicle.java
@@ -0,0 +1,105 @@
+package com.ps;
+
+import java.time.LocalDate;
+
+//vin|year|make|model|vehicleType|color|odometer|price
+public class Vehicle {
+    private String vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private int price;
+
+    public Vehicle(String vin, int year, String make, String model, String color, int odometer, String vehicleType, int price)
+    {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.odometer = odometer;
+        this.vehicleType = vehicleType;
+        this.price = price;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vin='" + vin + '\'' +
+                ", year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", vehicleType='" + vehicleType + '\'' +
+                ", color='" + color + '\'' +
+                ", odometer=" + odometer +
+                ", price=" + price +
+                '}';
+    }
+}
